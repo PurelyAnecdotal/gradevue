1bd655b1530540b53712c52f602bf2471167c1fc
Nonexistent-Name <58897346+Nonexistent-Name@users.noreply.github.com>
2023-12-31 22:14:19 -0800

    Improve type definitions

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -4,12 +4,12 @@ import type { StudentAccount } from '$lib/synergy';
 import type { Gradebook } from './Gradebook';
 import type { Attendance } from './Attendance';
 
-export const studentAccount: Writable<StudentAccount> = writable();
+export const studentAccount: Writable<StudentAccount | undefined> = writable();
 
-export const gradebook: Writable<Gradebook> | Writable<undefined> = writable(undefined);
+export const gradebook: Writable<Gradebook | undefined> = writable();
 
 export const gradebookLoaded = writable(false);
 
-export const attendance: Writable<Attendance> | Writable<undefined> = writable(undefined);
+export const attendance: Writable<Attendance | undefined> = writable();
 
 export const attendanceLoaded = writable(false);
