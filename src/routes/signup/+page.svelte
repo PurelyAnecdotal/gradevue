c0d281b0a1f7119042185c31baa17bfa7503a823
Nonexistent-Name <58897346+Nonexistent-Name@users.noreply.github.com>
2024-01-26 21:32:56 +0000

    Add signup message for SynergyPlus users

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -8,3 +8,7 @@
 	<Button color="light" class="w-full" href="/signup/google">with Google</Button>
 	<Button color="light" class="w-full" href="/signup/password">with a password</Button>
 </div>
+<span>
+	If you've used SynergyPlus before, you're already signed up. Just use the same password to
+	<a href="/login" class="text-primary-600 underline">log in</a>.
+</span>
