9ec850acdc9320a10e78ddb985848d3c617ea967
Nonexistent-Name <58897346+Nonexistent-Name@users.noreply.github.com>
2024-01-27 17:18:17 +0000

    Revert "add 404, update grades page"
    This reverts commit 4910c47ff08bdcfce6d88fdb2324b9aed2d51731.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,45 +6,21 @@ export default {
 	],
 	theme: {
 		extend: {
-		colors: {
-			primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
-		}
-		},
-		fontFamily: {
-		'body': [
-		'Inter', 
-		'ui-sans-serif', 
-		'system-ui', 
-		'-apple-system', 
-		'system-ui', 
-		'Segoe UI', 
-		'Roboto', 
-		'Helvetica Neue', 
-		'Arial', 
-		'Noto Sans', 
-		'sans-serif', 
-		'Apple Color Emoji', 
-		'Segoe UI Emoji', 
-		'Segoe UI Symbol', 
-		'Noto Color Emoji'
-	],
-		'sans': [
-		'Inter', 
-		'ui-sans-serif', 
-		'system-ui', 
-		'-apple-system', 
-		'system-ui', 
-		'Segoe UI', 
-		'Roboto', 
-		'Helvetica Neue', 
-		'Arial', 
-		'Noto Sans', 
-		'sans-serif', 
-		'Apple Color Emoji', 
-		'Segoe UI Emoji', 
-		'Segoe UI Symbol', 
-		'Noto Color Emoji'
-	]
+			colors: {
+				// flowbite-svelte
+				primary: {
+					50: '#FFF5F2',
+					100: '#FFF1EE',
+					200: '#FFE4DE',
+					300: '#FFD5CC',
+					400: '#FFBCAD',
+					500: '#FE795D',
+					600: '#EF562F',
+					700: '#EB4F27',
+					800: '#CC4522',
+					900: '#A5371B'
+				}
+			}
 		}
 	},
 	plugins: [require('flowbite/plugin')],
