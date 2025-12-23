301fa3a98ed80b638f0e45cac85fac10d9baefae
PurelyAnecdotal <58897346+PurelyAnecdotal@users.noreply.github.com>
2025-12-22 18:43:00 -1000

    Fix prerendering

--- src/routes/migrate/import/+page.svelte
+++ src/routes/migrate/import/+page.svelte
@@ -6,12 +6,14 @@
 	import { LocalStorageKey } from '$lib';
 	import { Button, Card } from 'flowbite-svelte';
 
-	const sourceOrigin = page.url.searchParams.get('origin');
+	let sourceOrigin: string | null | undefined = $state(undefined);
 
 	let hasExistingData = $state(false);
 
 	if (browser) {
 		hasExistingData = localStorage.getItem(LocalStorageKey.token) !== null;
+
+		sourceOrigin = page.url.searchParams.get('origin');
 
 		if (!hasExistingData) startMigration();
 	}
@@ -21,7 +23,7 @@
 
 		const allowedMigrationOrigins = env.PUBLIC_ALLOWED_MIGRATION_ORIGINS.split(',');
 
-		if (opener !== null && sourceOrigin !== null) {
+		if (opener !== null && sourceOrigin !== null && sourceOrigin !== undefined) {
 			(opener as Window).postMessage('ready', sourceOrigin);
 
 			addEventListener('message', ({ origin, data, source }) => {
@@ -39,21 +41,26 @@
 
 <div class="flex min-h-screen min-w-screen items-center justify-center">
 	<Card class="space-y-4 dark:text-gray-200">
-		{#if sourceOrigin !== null}
-			<h1 class="text-xl">Migrating user data from {sourceOrigin}...</h1>
-
-			{#if hasExistingData}
-				<p>You already have data here. Would you like to overwrite it?</p>
-
-				<div class="flex gap-2">
-					<Button href="/grades" color="light" class="w-full">Cancel</Button>
-					<Button onclick={startMigration} color="red" class="w-full cursor-pointer">
-						Overwrite
-					</Button>
-				</div>
+		<h1 class="text-xl">
+			{#if env.PUBLIC_ALLOWED_MIGRATION_ORIGINS !== undefined}
+				{#if sourceOrigin !== null}
+					Migrating user data from {sourceOrigin}...
+				{:else}
+					Cannot migrate user data: source origin unspecified
+				{/if}
+			{:else}
+				Cannot migrate user data: no migration origins allowed
 			{/if}
-		{:else}
-			<h1 class="text-xl">Cannot migrate user data: source origin unspecified</h1>
+		</h1>
+		{#if env.PUBLIC_ALLOWED_MIGRATION_ORIGINS !== undefined && sourceOrigin !== null && hasExistingData}
+			<p>You already have data here. Would you like to overwrite it?</p>
+
+			<div class="flex gap-2">
+				<Button href="/grades" color="light" class="w-full">Cancel</Button>
+				<Button onclick={startMigration} color="red" class="w-full cursor-pointer">
+					Overwrite
+				</Button>
+			</div>
 		{/if}
 	</Card>
 </div>
